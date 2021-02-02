e784b0bc2dec6b4695baea1bc99896477e525da7
Bleak Chandler <bleakchandler@gmail.com>
2021-02-02 12:49:05 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,4 @@
-class Dog
+class Dog #Think of instance variables as the containers for instance-specific information
     def name=(dog_name)
        @this_dogs_name = dog_name #The value held by an instance variable is specific to whatever instance of the class it happens to belong to.
     end
