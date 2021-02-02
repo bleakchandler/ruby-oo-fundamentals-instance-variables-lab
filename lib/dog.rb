846ced345674e474b02e20672989d37e23a4fc81
Bleak Chandler <bleakchandler@gmail.com>
2021-02-02 12:43:58 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+    def name=(dog_name)
+       @this_dogs_name = dog_name #The value held by an instance variable is specific to whatever instance of the class it happens to belong to.
+    end
+
+    def name
+       @this_dogs_name
+    end
+end
+
+lassie = Dog.new #Creates a new dog instance, give it a name and tries to access- or read- its name.
+lassie.name = "Lassie"
+
+puts lassie.name
